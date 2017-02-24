b047cb2bdb35afc9d5c4f691b7c47d4d61327780
jg891159 <jg891159@C39845.ens.uqam.ca>
2017-02-24 14:10:02 -0500

    Ajout et modification de methode pour traiter les montants avec virgule

--- src/projetagile/InterfaceContrat.java
+++ src/projetagile/InterfaceContrat.java
@@ -71,11 +71,31 @@ public abstract class InterfaceContrat {
     public static double convertirStringEnDouble(String strMontant){
         double montant;
         String stringSansDollar = strMontant.replace("$", "");
+        
+        if(contientVirgule(stringSansDollar)){
+            stringSansDollar = stringSansDollar.replace(",", ".");
+        }
+        
         montant = Double.parseDouble(stringSansDollar);
         
         return montant;
     }
     
+    
+    //methode pour savoir s'il faut remplacer la virgule par un point
+    //dans le montant
+    public static boolean contientVirgule(String montant){
+        boolean reponse = false;
+        for(int i = 0; i < montant.length(); i++){
+            if(montant.charAt(i) == ','){
+                reponse = true;
+            } 
+        }
+        
+        return reponse;
+    }
+            
+            
     public static String convertirDoubleEnString(double dblMontant){
         String montant;
         
