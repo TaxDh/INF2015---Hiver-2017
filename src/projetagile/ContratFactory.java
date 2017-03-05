f9736feca8f037038d0f979d3b2236829664ce90
TaxDh <dhmo123@hotmail.com>
2017-03-05 04:43:47 -0500

    ajout de commentaire expliquant ce que fait la classe ContratFactory

--- src/projetagile/ContratFactory.java
+++ src/projetagile/ContratFactory.java
@@ -1,16 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ca choisi le contrat
  */
 package projetagile;
 
 import projetagile.jsonmodels.ModeleJsonIn;
 
-/**
- *
- * @author rene
- */
+
 public class ContratFactory {
     public static InterfaceContrat instancieContrat(ModeleJsonIn modele){
         char typeContrat = modele.getTypeContrat();
