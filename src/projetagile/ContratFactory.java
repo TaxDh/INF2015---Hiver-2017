716573817192513c88ba82066e96bf8d55dfb6e7
TaxDh <dhmo123@hotmail.com>
2017-03-26 23:33:19 -0400

    effacement de commentaire dans contratFactory

--- src/projetagile/ContratFactory.java
+++ src/projetagile/ContratFactory.java
@@ -1,16 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Choisi quel contrat a traiter
  */
 package projetagile;
 
 import projetagile.jsonmodels.ModeleJsonIn;
 
-/**
- *
- * @author rene
- */
 public class ContratFactory {
     public static InterfaceContrat instancieContrat(ModeleJsonIn modele){
         char typeContrat = modele.getTypeContrat();
