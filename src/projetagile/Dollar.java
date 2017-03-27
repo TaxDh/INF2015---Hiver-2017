912ac2fff81c71dd8e97063b16580bf8bfafe9d1
TaxDh <dhmo123@hotmail.com>
2017-03-26 23:35:45 -0400

    effacement de commentaire et ajout de commentaire dans dollar et invalidArgument

--- src/projetagile/Dollar.java
+++ src/projetagile/Dollar.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Objet Dollar pour faire les calculs de montant
  */
 package projetagile;
 
-/**
- *
- * @author kf891141
- */
 public class Dollar {    
     
     private int montant;
--- src/projetagile/InvalidArgumentException.java
+++ src/projetagile/InvalidArgumentException.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Traite les erreurs lorsqu'il y en a
  */
 package projetagile;
 
-/**
- *
- * @author rene
- */
 public class InvalidArgumentException extends Exception {
 
     public InvalidArgumentException(String message) {
