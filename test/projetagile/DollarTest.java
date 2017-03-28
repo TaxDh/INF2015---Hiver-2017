9ba1f57ac52ce77bfb7e29c13c465250d29891c7
farahsaf <farah.saf@hotmail.com>
2017-03-28 14:42:07 -0400

    Ajout fonctions de test dans dollartest

--- test/projetagile/DollarTest.java
+++ test/projetagile/DollarTest.java
@@ -5,13 +5,14 @@
  */
 package projetagile;
 
-import java.io.IOException;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 /**
  *
@@ -265,3 +266,4 @@ public class DollarTest {
     }
     
 }
+
