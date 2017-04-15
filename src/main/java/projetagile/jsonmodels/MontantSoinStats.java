9d8562c56b3f1029d8cc170e5bcb483e7d0856c2
TaxDh <dhmo123@hotmail.com>
2017-04-15 18:29:53 -0400

    reintroduction du style, il avait disparu... (???)

--- src/main/java/projetagile/jsonmodels/MontantSoinStats.java
+++ src/main/java/projetagile/jsonmodels/MontantSoinStats.java
@@ -3,6 +3,19 @@
  */
 package projetagile.jsonmodels;
 
+import projetagile.Dollar;
+
 public class MontantSoinStats {
+    private Dollar montantMaxMassotherapie;
+    private Dollar montantMaxOsteopathie;
+    private Dollar montantMaxkinesitherapie;
+    private Dollar montantMaxMGP;
+    private Dollar montantMaxPsychologie;
+    private Dollar montantMaxDentaire;
+    private Dollar montantMaxNaturo;
+    private Dollar montantMaxChiropratie;
+    private Dollar montantMaxPhysiotherapie;
+    private Dollar montantMaxOrthophonie;    
+    
     
 }
